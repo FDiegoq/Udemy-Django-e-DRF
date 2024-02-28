da72228fa81b3b234f498d12ed6e2b60fac8d716
Diego <119186100+FDiegoq@users.noreply.github.com>
2024-02-28 19:40:16 -0300

    adjusting urls
    ajustando urls e dinamizando a navegação das receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes)
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
