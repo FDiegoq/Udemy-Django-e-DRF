28ed3b99390117ddb758db600032c4dcd21e71ea
Diego <119186100+FDiegoq@users.noreply.github.com>
2024-03-03 17:48:59 -0300

    adding classes
    using admin.py and using the django admin to create classes and also real datas

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name=models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -17,7 +20,7 @@ class Recipe(models.Model):
     preparation_step_is_html=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateField(auto_now=True)
-    is_published=models.BooleanField(False)
+    is_published=models.BooleanField(default=False)
     cover=models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category=models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
@@ -25,3 +28,6 @@ class Recipe(models.Model):
     author=models.ForeignKey(
         User, on_delete= models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
