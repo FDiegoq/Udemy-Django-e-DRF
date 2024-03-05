071075139ee2a32855480227ef229647f99c8583
Diego <119186100+FDiegoq@users.noreply.github.com>
2024-03-05 19:33:49 -0300

    ajuste em caso de falta de dados

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="recipes-home"),
     path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="recipes-category"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 # Create your views here.
 
 def home(request):
+    recipe=Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipe
     })
+
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe_view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
     })
